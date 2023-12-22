b8146fdef8c64ec172cf93524b1fe8c62703ec90
Martin Ortiz <leo.martin178@gmail.com>
2023-12-21 20:32:41 -0600

    feture: se crea endpoint para registrar usuario.}

---
+++ src/main/java/com/fashionlike/PooRsmApplication.java
@@ -0,0 +1,13 @@
+package com.fashionlike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooRsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooRsmApplication.class, args);
+	}
+
+}
